0207eb1cfdd4cbd3527380ab2b22e205fbe40fc5
vkilicaslan <v.kilicaslan@etibilisim.com>
2021-05-09 05:09:08 +0300

    20210509 E-Arşiv access control has been fixed

--- drupal/web/modules/custom/aydinlik/src/EventSubscriber/NodeAccessSubscriber.php
+++ drupal/web/modules/custom/aydinlik/src/EventSubscriber/NodeAccessSubscriber.php
@@ -64,7 +64,7 @@ class NodeAccessSubscriber implements EventSubscriberInterface {
     $this->messenger = \Drupal::messenger();
     $route_match = RouteMatch::createFromRequest($event->getRequest());
     if (($node = $route_match->getParameter('node')) && $node instanceof NodeInterface) {
-      //if (!\Drupal::service('path.matcher')->isFrontPage() || ($node->bundle() != 'page' || $node->bundle() != 'webform')) {
+      if (!\Drupal::service('path.matcher')->isFrontPage() || ($node->bundle() != 'page' || $node->bundle() != 'webform')) {
       if ($node->bundle() == 'e_dergi') {
         if ($this->current_user->isAnonymous()) {
           $this->messenger->addMessage($config->get('girisyapmesaji'));
@@ -109,21 +109,35 @@ class NodeAccessSubscriber implements EventSubscriberInterface {
             $redirect = new RedirectResponse('/e-gazete-aboneligi');
             $redirect->send();
           }
-          if ($node->bundle() == 'e_arsiv') {
-            $subscription_type = $this->current_user->get('field_abonelik_turu');
-            $subscription_type_count = $subscription_type->count();
-            if ($subscription_type_count < 2){
-              $earchives_subscription = Term::load($this->current_user->field_abonelik_turu->referencedEntities()[0]->tid->value);
-              if (!str_contains($earchives_subscription->getName(), 'E-Arşiv')) {
-                $this->messenger->addWarning($config->get('earsivabonesidegilmesaji'));
-                $redirect = new RedirectResponse('/e-gazete-aboneligi');
-                $redirect->send();
+        }
+      }
+      if ($node->bundle() == 'e_arsiv') {
+        if ($this->current_user->isAnonymous()) {
+          $this->messenger->addMessage($config->get('girisyapmesaji'));
+          $redirect = new RedirectResponse($login->toString());
+          $event->setResponse($redirect);
+        }
+        elseif (!$this->current_user->hasPermission('bypass permission checks')) {
+            if ($this->current_user->hasRole('abone')) {
+              $subscription_type = $this->current_user->get('field_abonelik_turu');
+              $subscription_type_count = $subscription_type->count();
+              if ($subscription_type_count < 2){
+                $earchives_subscription = Term::load($this->current_user->field_abonelik_turu->referencedEntities()[0]->tid->value);
+                if (!str_contains($earchives_subscription->getName(), 'E-Arşiv')) {
+                  $this->messenger->addWarning($config->get('earsivabonesidegilmesaji'));
+                  $redirect = new RedirectResponse('/e-gazete-aboneligi');
+                  $redirect->send();
+                }
               }
+            }
+            else {
+              $this->messenger->addWarning($config->get('earsivabonesidegilmesaji'));
+              $redirect = new RedirectResponse('/e-gazete-aboneligi');
+              $redirect->send();
             }
           }
         }
       }
     }
   }
-
 }
